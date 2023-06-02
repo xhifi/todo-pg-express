a6ba403409f8758c85796ead7041cb2557a55da6
xhifi <shifa.newversion@gmail.com>
2023-06-02 22:18:20 +0500

    corrected seeding

--- controllers/migrations/seedUp.js
+++ controllers/migrations/seedUp.js
@@ -1,20 +1,21 @@
 const db = require("../../utils/db")();
-const fs = require("fs");
-const path = require("path");
 
 module.exports = (req, res) => {
-  db.query(
-    require("fs")
-      .readFileSync(
-        fs.readFileSync(path.join(`${process.cwd()}/seed.sql`)).toString()
-      )
-      .toString(),
-    (err, result) => {
-      if (err)
-        return res
-          .status(500)
-          .send({ error: err, message: "Something went wrong while querying" });
-      res.send(result.command);
-    }
-  );
+  db.query(require("fs").readFileSync(`${process.cwd()}/seed.sql`).toString())
+    .then((result) => {
+      let response = [];
+      result.forEach((query, index) => {
+        response.push({ id: index, status: query.command });
+      });
+      res.status(201).json(response);
+    })
+    .catch((e) => {
+      if (e.code === "42P07") {
+        return res.status(400).json({
+          msg: "One or more tables in the query already exist in database.",
+          err: e,
+        });
+      }
+      return res.status(500).json({ msg: `Can't seed up`, err: e });
+    });
 };
